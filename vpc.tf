c6fcc5a19b721b87e1383231a2b34486a6d42c31
Divyang Patel <divyang.jp@gmail.com>
2022-04-22 12:28:41 +1000

    remove external IP not required atm

--- vpc.tf
+++ vpc.tf
@@ -39,22 +39,6 @@ resource "google_compute_subnetwork" "data" {
   private_ip_google_access = true
 }
 
-resource "google_compute_global_address" "private-ip-peering" {
-  name          = "google-managed-services-custom"
-  purpose       = "VPC_PEERING"
-  address_type  = "INTERNAL"
-  prefix_length = 24
-  network       = google_compute_network.custom.id
-}
-
-resource "google_service_networking_connection" "private-vpc-connection" {
-  network = google_compute_network.custom.id
-  service = "servicenetworking.googleapis.com"
-  reserved_peering_ranges = [
-    google_compute_global_address.private-ip-peering.name
-  ]
-}
-
 # Global External IPv4 used with HTTP(s) load balancer
 resource "google_compute_global_address" "external-ipv4" {
   project      = var.project
